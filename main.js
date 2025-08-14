0a03d0ba47de8d1ef8922ed896cdfe35ed07ef00
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-08-15 00:14:04 +0700

    USE FETCH

--- main.js
+++ main.js
@@ -1,57 +1,41 @@
-function sendRequest(method = "GET", url) {
-  //trả về hàm giá trị resolve và reject
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url, true);
-    //gửi iu cầu
-    xhr.send();
-    //lọc phản hồi trở về
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 400) {
-        //dung thi trar ve resolve
-        const contentType = xhr.getResponseHeader("content-type");
-        const isJson = contentType && contentType.includes("application/json");
-        if (isJson) {
-          try {
-            resolve(JSON.parse(xhr.responseText));
-          } catch (error) {
-            console.log("Invalid JSON format");
-          }
-        } else {
-          resolve(xhr.responseText);
-        }
-      } else {
-        //sai thi reject
-        reject(`HTTP code : ${xhr.status}`);
-      }
-    };
-    xhr.onerror = function () {
-      console.error("Network error");
-    };
-  }); //Khởi tạo bắt đầu gọi
+//CÁCH DÙNG FETCH , NÓ LUÔN MẶC ĐỊNH TRẢ VỀ 1 RESPONSE DÙ CÓ CHẠY ĐƯỢC HAY KO
+async function send(url, method) {
+  // bản chất của fetch VẪN LÀ 1 PROMISE NÊN CÓ THỂ DÙNG ĐƯỢC ASYNC
+  const res = await fetch(url, { method });
+  console.log(res);
+  if (!res.ok) {
+    throw new Error(`HTTP CODE ERROR: ${res.status}`);
+  }
+  const content = res.headers.get("content-type");
+  const isJson = content && content.includes("application/json");
+  return isJson ? res.json() : res.text();
 }
 const head = document.querySelector(".header");
-sendRequest("GET", "./partials/header.html").then((responseText) => {
-  head.innerHTML = responseText;
-});
-function firstProvince() {
-  return sendRequest(
-    "GET",
-    "https://api01.f8team.dev/api/address/provinces"
-  ).then((result) => result.data[0]);
-}
-function firstDistrict(firstProvince) {
-  return sendRequest(
-    "GET",
-    `https://api01.f8team.dev/api/address/districts?province_id=${firstProvince}`
-  ).then((result) => result.data[0]);
-}
-function firstWard(firstDistrict) {
-  return sendRequest(
-    "GET",
-    `https://api01.f8team.dev/api/address/wards?district_id=${firstDistrict}`
-  ).then((result) => result.data[0]);
-}
+send("./partials/header.html")
+  .then((responseText) => {
+    head.innerHTML = responseText;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+// function firstProvince() {
+//   return sendRequest(
+//     "GET",
+//     "https://api01.f8team.dev/api/address/provinces"
+//   ).then((result) => result.data[0]);
+// }
+// function firstDistrict(firstProvince) {
+//   return sendRequest(
+//     "GET",
+//     `https://api01.f8team.dev/api/address/districts?province_id=${firstProvince}`
+//   ).then((result) => result.data[0]);
+// }
+// function firstWard(firstDistrict) {
+//   return sendRequest(
+//     "GET",
+//     `https://api01.f8team.dev/api/address/wards?district_id=${firstDistrict}`
+//   ).then((result) => result.data[0]);
+// }
 // firstProvince()
 //   .then((firstProvince) => firstDistrict(firstProvince.province_id))
 //   .then((firstDistrict) => firstWard(firstDistrict.district_id))
@@ -110,12 +94,13 @@ function firstWard(firstDistrict) {
 //Promise.reject();
 async function handle() {
   try {
-    const province=await firstProvince();
-    const district=await firstDistrict(province.province_id);
-    const ward=await firstWard(district.district_id);
-    console.log(ward)
+    const province = await firstProvince();
+    const district = await firstDistrict(province.province_id);
+    const ward = await firstWard(district.district_id);
+    console.log(ward);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
-handle(); // phải gọi thì code mới chạy
+handle();
+// phải gọi thì code mới chạy
