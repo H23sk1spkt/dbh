2183728cf3c5189fdba9aa2149bd802642699001
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-08-28 19:37:42 +0700

    use dynamic import ( second usage)

--- utils/main3.js
+++ utils/main3.js
@@ -1,8 +1,7 @@
 // CÁCH SỬ DỤNG IMPORT ĐỘNG
 const button=document.querySelector("#button")
-button.addEventListener("click",()=>{
-    import("./index.js").then((module)=>{
-        console.log(module.calculation(1,2))
-    })
+button.addEventListener("click", async ()=>{
+    const {calculation}= await import('./index.js');
+    console.log(calculation(1,2))
 })
 //sử dụng dynamic import sẽ giúp cho tối ưu khi nào cần mới dùng , sẽ tối ưu thời gian và hiệu suất của trang web
