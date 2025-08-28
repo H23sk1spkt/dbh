2402a1ea210e43606aee6943772b15e12c69e35f
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-08-28 17:43:02 +0700

    module

---
+++ main1.js
@@ -0,0 +1,3 @@
+//lấy imformation từ main2
+import { helo,math } from "./main2.js";
+console.log(math);
---
+++ main2.js
@@ -0,0 +1,7 @@
+// khai bao bien trong module phu
+const helo = "toi la hai ne heheh";
+function math(x,y){
+    return x+y
+}
+// cho information ở main2
+export { helo,math };
