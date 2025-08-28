2734b384a5f16a2f376a418b4c892a064f35269a
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-08-29 00:19:20 +0700

    how to write function contructor for standare ES6 ( if the function has a value passed in

--- utils/main5.js
+++ utils/main5.js
@@ -12,14 +12,26 @@
 //trên là cách thông thường
 //giờ đến cách ngắn gọn es6;
 //ĐÂY LÀ CÁCH VIẾT KHI KHỒNG TRUYỀN GIÁ TRỊ GÌ VÀO HẾT
-class Kozuro{
-    value=0;
+// class Kozuro{
+//     value=0;
+//     increase(){
+//         this.value++;
+//     }
+// }
+// const gau=new Kozuro()
+// console.log(gau.value);
+// gau.increase();
+// console.log(gau.value)
+//ĐÂY LÀ CÁCH VIẾT NẾU MUỐN TRUYỀN GIÁ TRỊ VÀO
+class Minamoto{
+    constructor(init){
+        this.value=init
+    }
     increase(){
-        this.value++;
+        this.value++
     }
 }
-const gau=new Kozuro()
+const gau=new Minamoto(100)
 console.log(gau.value);
 gau.increase();
-console.log(gau.value)
-//ĐÂY LÀ CÁCH VIẾT NẾU MUỐN TRUYỀN GIÁ TRỊ VÀO
+console.log(gau.value);
